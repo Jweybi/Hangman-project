99f7b48e6a6e9f31d8b99281e7c684e3bab7f43a
Jweybi <jweybi09@gmail.com>
2026-03-06 21:12:59 +0800

    Update HangmanMain.java

--- Hangman/Hangman/src/HangmanMain.java
+++ Hangman/Hangman/src/HangmanMain.java
@@ -1,13 +1,9 @@
-/*Dito pre ginawa ko toh para dito natin ilalagay final code natin bale code muna
-kayo sa vscode bago nyo ilapag dito para ma run nyo wala kaseng run dito*/
-
-public class Main {
+public class HangmanMain {
     public static void main(String[] args) throws Exception {
-        
-        HangmanGame game = new HangmanGame();
 
-        game.Startgame();
+        GameLogic game = new GameLogic();
+
+        game.startGame();
     }
 }
-
 
