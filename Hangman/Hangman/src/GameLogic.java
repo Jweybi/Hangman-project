aca92d5f302cf25e28bb01191cbea629ca3138ca
Jweybi <jweybi09@gmail.com>
2026-03-06 20:36:27 +0800

    Update GameLogic.java

---
+++ Hangman/Hangman/src/GameLogic.java
@@ -0,0 +1,17 @@
+/*Sa IDE muna kayo edit mga tol bago mag lapag dito*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*Dito na yung game logi mga tol */
+public class GameLogic {
+    
+    private String[] words = {"NetBeans", "Java", "Inheritance", "Subclass", "MainClass"};
+    private String secretword;
+    private char[] guessedword;
+    private int attempts = 6;
+    
+    private Scanner scanner = new Scanner(System.in);
+
+    
+}
